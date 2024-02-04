5fe8375a250758c40e7e9c85946227f4733be055
ladmael <don.sosiska339@gmail.com>
2024-02-04 17:28:13 +0200

    resolved

--- task.sql
+++ task.sql
@@ -1,1 +1,7 @@
--- write your code for database user creation here
+-- Create webappuser with basic CRUD permissions
+CREATE USER 'webappuser'@'%' IDENTIFIED BY 'P@ssw0rd';
+GRANT SELECT, INSERT, UPDATE, DELETE ON ShopDB.* TO 'webappuser'@'%';
+
+-- Create deploymentuser with full permissions
+CREATE USER 'deploymentuser'@'%' IDENTIFIED BY 'P@ssw0rd';
+GRANT ALL PRIVILEGES ON ShopDB.* TO 'deploymentuser'@'%';
