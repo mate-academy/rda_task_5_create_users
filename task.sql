b33090da77b93b1e03ce1b0a01298be49e65521a
AlexHCom <alexhcomgrit@gmail.com>
2024-02-09 00:28:27 +0200

    Solution

--- task.sql
+++ task.sql
@@ -1,1 +1,8 @@
 -- write your code for database user creation here
+-- Create webappuser
+CREATE USER 'webappuser'@'%' IDENTIFIED BY 'P@ssw0rd';
+GRANT SELECT, INSERT, UPDATE, DELETE ON ShopDB.* TO 'webappuser'@'%';
+
+-- Create deploymentuser
+CREATE USER 'deploymentuser'@'%' IDENTIFIED BY 'P@ssw0rd';
+GRANT ALL PRIVILEGES ON ShopDB.* TO 'deploymentuser'@'%';
