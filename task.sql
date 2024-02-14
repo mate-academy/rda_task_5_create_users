36b7d31f7a33ba04c31e20d25d9556008bc75d55
Dmytry-S <sviridoffd@gmail.com>
2024-02-14 10:50:13 +0200

    Update user's grants

--- task.sql
+++ task.sql
@@ -2,6 +2,5 @@
 CREATE USER 'webappuser'@'%' IDENTIFIED BY 'P@ssw0rd';
 CREATE USER 'deploymentuser'@'%' IDENTIFIED BY 'P@ssw0rd';
 
-USE ShopDB;
-GRANT INSERT, UPDATE, DELETE, SELECT ON ShopDB.Countries TO 'webappuser'@'%';
+GRANT INSERT, UPDATE, DELETE, SELECT ON ShopDB.* TO 'webappuser'@'%';
 GRANT ALL ON ShopDB.* TO 'deploymentuser'@'%';
