451e54308275de081755cb09b03328567d3b601a
Darya-Savchenko <savchenkodarya277@gmail.com>
2024-07-17 20:04:39 +0300

    fix GRANT command for webappuser user

--- task.sql
+++ task.sql
@@ -1,5 +1,5 @@
 CREATE USER 'webappuser'@'%' IDENTIFIED BY 'P@ssw0rd';
-GRANT INSERT, UPDATE, DELETE, SELECT on ShopDB.* TO 'deploymentuser'@'%';
+GRANT INSERT, UPDATE, DELETE, SELECT on ShopDB.* TO 'webappuser'@'%';
 
 CREATE USER 'deploymentuser'@'%' IDENTIFIED BY 'P@ssw0rd';
 GRANT ALL PRIVILEGES on ShopDB.* TO 'deploymentuser'@'%';
