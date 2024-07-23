7205f8dbdae3890002a5ba88266356fd577924c9
Yakov K <hyakunosuke7@gmail.com>
2024-07-23 16:15:50 +0300

    CreateUserSolution

--- task.sql
+++ task.sql
@@ -1,1 +1,5 @@
--- write your code for database user creation here
+CREATE USER 'webappuser'@'%' IDENTIFIED BY 'P@ssw0rd';
+GRANT INSERT, SELECT, UPDATE, DELETE ON Shop.DB.* TO 'webappuser'@'%';
+
+CREATE USER 'deploymentuser'@'%' IDENTIFIED BY 'P@ssw0rd';
+GRANT ALL ON Shop.DB.* TO 'deploymentuser'@'%';
