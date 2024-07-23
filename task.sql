d51e5e442ca7b1b565940153110e6387717bcf37
Yakov K <hyakunosuke7@gmail.com>
2024-07-23 16:20:07 +0300

    CreateUserSolutionFix2

--- task.sql
+++ task.sql
@@ -2,4 +2,4 @@ CREATE USER 'webappuser'@'%' IDENTIFIED BY 'P@ssw0rd';
 GRANT INSERT, SELECT, UPDATE, DELETE ON ShopDB.* TO 'webappuser'@'%';
 
 CREATE USER 'deploymentuser'@'%' IDENTIFIED BY 'P@ssw0rd';
-GRANT ALL ON Shop.DB.* TO 'deploymentuser'@'%';
+GRANT ALL ON ShopDB.* TO 'deploymentuser'@'%';
