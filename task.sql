8f36c0186964b6c02e6832938fc6bd3922e5604c
volry <v.v.ryzhuk@gmail.com>
2024-08-02 19:11:35 +0300

    create users

--- task.sql
+++ task.sql
@@ -1,1 +1,13 @@
 -- write your code for database user creation here
+USE ShopDB;
+
+-- Create webappuser (I frequently do the same on MS SQL server ))
+CREATE USER 'webappuser'@'%' IDENTIFIED BY 'P@ssw0rd';
+GRANT SELECT, INSERT, UPDATE, DELETE ON ShopDB.* TO 'webappuser'@'%';
+
+-- Create the deploymentuser
+CREATE USER 'deploymentuser'@'%' IDENTIFIED BY 'P@ssw0rd';
+GRANT ALL PRIVILEGES ON ShopDB.* TO 'deploymentuser'@'%';
+
+-- Apply the changes
+FLUSH PRIVILEGES;
