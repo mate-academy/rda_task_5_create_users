395c4f3fc7cfaf5ab3969095cf486e50e9ee7aad
Alex Zhidkov <alex.z@mate.academy>
2024-08-04 10:47:08 +0300

    solution added

--- task.sql
+++ task.sql
@@ -1,9 +1,5 @@
--- write your code for database user creation here
-
 CREATE USER 'webappuser'@'%' IDENTIFIED BY 'P@ssw0rd';
 CREATE USER 'deploymentuser'@'%' IDENTIFIED BY 'P@ssw0rd';
-
--- following code to be run through the terminal via SSH or direct input
 
 GRANT SELECT, INSERT, UPDATE, DELETE ON ShopDB.* TO 'webappuser'@'%';
 GRANT ALL ON ShopDB.* TO 'deploymentuser'@'%';
