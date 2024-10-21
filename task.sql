55c3149d17f9129cc9772a9e702c224fea02bfbf
Ivan Bilobrov <ivanbilobrov07@gmail.com>
2024-10-22 02:40:47 +0300

    fix

--- task.sql
+++ task.sql
@@ -5,3 +5,5 @@ GRANT SELECT, INSERT, UPDATE, DELETE ON ShopDB.* TO 'webappuser'@'%';
 
 CREATE USER 'deploymentuser'@'%' IDENTIFIED BY 'P@ssw0rd';
 GRANT ALL PRIVILEGES ON ShopDB.* TO 'deploymentuser'@'%';
+
+FLUSH PRIVILEGES;
