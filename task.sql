d42d1dced4b1d4044ea6c0ce291ab0c909cb7b10
Andrii Levchenko <levchenkoandrii11@gmail.com>
2024-10-29 15:43:15 +0200

    Updated webappuser permissions

--- task.sql
+++ task.sql
@@ -1,5 +1,5 @@
 CREATE USER 'webappuser'@'%' IDENTIFIED BY 'P@ssw0rd';
-GRANT CREATE, SELECT, UPDATE, DELETE ON ShopDB.* TO 'webappuser'@'%';
+GRANT SELECT, INSERT, UPDATE, DELETE ON ShopDB.* TO 'webappuser'@'%';
 
 CREATE USER 'deploymentuser'@'%' IDENTIFIED BY 'P@ssw0rd';
 GRANT ALL PRIVILEGES ON ShopDB.* TO 'deploymentuser'@'%';
