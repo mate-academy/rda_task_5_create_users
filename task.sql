59bcef1e02c95bd67718b250ceeb3f8a72ece55e
Nazar Makivchuk <nazar.makivchuk@gmail.com>
2024-10-29 17:52:06 +0200

    create users

--- task.sql
+++ task.sql
@@ -1,1 +1,7 @@
 -- write your code for database user creation here
+sudo mysql -u root
+CREATE USER 'webappuser'@'%' INDENTIFIED BY 'P@ssw0rd';
+GRANT INSERT, UPDATE, DELETE, SELECT ON ShopDB.* TO 'webappuser'@'%'
+
+CREATE USER 'deploymentuser'@'%'IDENTIFIED BY 'P@ssw0rd';
+GRANT ALL ON ShopDB.* TO 'deploymentuser'@'%';
