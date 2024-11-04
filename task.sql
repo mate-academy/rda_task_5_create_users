8b2a5054c7b742a95e3115d31bb7667df66dc4cb
Gariman <garimanblack1@gmail.com>
2024-11-04 21:31:16 +0200

    Solution Create users

--- task.sql
+++ task.sql
@@ -1,1 +1,11 @@
--- write your code for database user creation here
+DELIMITER //
+
+-- Creating users
+CREATE USER 'webappuser'@'%' IDENTIFIED BY 'P@ssw0rd';
+CREATE USER 'deploymentuser'@'%' IDENTIFIED BY 'P@ssw0rd';
+
+-- Assigning privileges
+GRANT SELECT, INSERT, UPDATE, DELETE ON ShopDB.* TO 'webappuser'@'%';
+GRANT ALL ON ShopDB.* TO 'deploymentuser'@'%';
+
+DELIMITER ;
