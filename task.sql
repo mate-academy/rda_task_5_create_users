bfb75a97a73f8c3d7e893479a984c8ea366c308a
L1mbxbx <ilya.msd2003@gmail.com>
2024-12-02 15:10:19 +0200

    Add user creation script

--- task.sql
+++ task.sql
@@ -1,1 +1,9 @@
--- write your code for database user creation here
+USE ShopDB;
+
+-- Create the web application user
+CREATE USER 'webappuser'@'%' IDENTIFIED BY 'P@ssw0rd';
+GRANT SELECT, INSERT, UPDATE, DELETE ON ShopDB.* TO 'webappuser'@'%';
+
+-- Create the deployment user
+CREATE USER 'deploymentuser'@'%' IDENTIFIED BY 'P@ssw0rd';
+GRANT ALL PRIVILEGES ON ShopDB.* TO 'deploymentuser'@'%';
