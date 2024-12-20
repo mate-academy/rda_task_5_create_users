4d56c94e3adb265507039c4f548546562d9a4353
zaeba <zaebalsya13@gmail.com>
2024-12-20 11:20:46 +0200

    54

--- task.sql
+++ task.sql
@@ -1,1 +1,13 @@
--- write your code for database user creation here
+-- Use the ShopDB database
+USE ShopDB;
+
+-- Create webappuser with limited CRUD permissions
+CREATE USER 'webappuser'@'%' IDENTIFIED BY 'P@ssw0rd';
+GRANT SELECT, INSERT, UPDATE, DELETE ON ShopDB.* TO 'webappuser'@'%';
+
+-- Create deploymentuser with all privileges
+CREATE USER 'deploymentuser'@'%' IDENTIFIED BY 'P@ssw0rd';
+GRANT ALL PRIVILEGES ON ShopDB.* TO 'deploymentuser'@'%';
+
+-- Apply changes to ensure permissions are updated
+FLUSH PRIVILEGES;
