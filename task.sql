64bb92e220f4bb8899436117b4e6d4d3a8a58602
zaeba <zaebalsya13@gmail.com>
2024-12-20 11:26:25 +0200

    545

--- task.sql
+++ task.sql
@@ -2,12 +2,18 @@
 USE ShopDB;
 
 -- Create webappuser with limited CRUD permissions
-CREATE USER 'webappuser'@'%' IDENTIFIED BY 'P@ssw0rd';
+CREATE USER IF NOT EXISTS 'webappuser'@'%' IDENTIFIED BY 'P@ssw0rd';
 GRANT SELECT, INSERT, UPDATE, DELETE ON ShopDB.* TO 'webappuser'@'%';
 
--- Create deploymentuser with all privileges
-CREATE USER 'deploymentuser'@'%' IDENTIFIED BY 'P@ssw0rd';
+-- Create deploymentuser with full privileges
+CREATE USER IF NOT EXISTS 'deploymentuser'@'%' IDENTIFIED BY 'P@ssw0rd';
 GRANT ALL PRIVILEGES ON ShopDB.* TO 'deploymentuser'@'%';
 
 -- Apply changes to ensure permissions are updated
 FLUSH PRIVILEGES;
+
+-- Verify permissions for webappuser
+SHOW GRANTS FOR 'webappuser'@'%';
+
+-- Verify permissions for deploymentuser
+SHOW GRANTS FOR 'deploymentuser'@'%';
