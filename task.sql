210fca8f681d074080a7d62da913edf827978cd9
Samuel Morgan <samuel.edmund.morgan@icloud.com>
2025-01-02 15:31:17 +0200

    Solution

--- task.sql
+++ task.sql
@@ -3,4 +3,5 @@ CREATE USER 'webappuser'@'%' IDENTIFIED BY 'P@ssw0rd';
 GRANT SELECT, INSERT, UPDATE, DELETE ON ShopDB.* TO 'webappuser'@'%';
 
 CREATE USER 'deploymentuser'@'%' IDENTIFIED BY 'P@ssw0rd';
-GRANT ALL ON ShopDB.* TO 'deploymentuser'@'%';
+GRANT ALL PRIVILEGES ON ShopDB.* TO 'deploymentuser'@'%';
+
