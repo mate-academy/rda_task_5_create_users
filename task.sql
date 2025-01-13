f89f26353919d666c67a85c8e92ed0f44a8aa393
Greena1c <shchedrykserhii@gmail.com>
2025-01-13 02:11:22 +0200

    Solutio:1

--- task.sql
+++ task.sql
@@ -1,8 +1,9 @@
 CREATE USER 'webappuser'@'%' IDENTIFIED BY 'P@ssw0rd';
-GRANT SELECT, INSERT, UPDATE, DELETE
-ON ShopDB.* 
-TO 'webappuser'@'%';
 
+GRANT SELECT, INSERT, UPDATE, DELETE ON ShopDB.* TO 'webappuser'@'%';
 
 CREATE USER 'deploymentuser'@'%' IDENTIFIED BY 'P@ssw0rd';
-GRANT ALL PRIVILEGES ON *.* TO 'deploymentuser'@'%';
+
+GRANT ALL PRIVILEGES ON ShopDB.* TO 'deploymentuser'@'%';
+
+FLUSH PRIVILEGES;
